32c057efc26ae169239fe1041411ef32c50b4d14
Zhou-Siyu <1794278158@qq.com>
2020-04-17 22:22:09 +0800

    2020作业

--- datetime-1.cpp
+++ datetime-1.cpp
@@ -1,42 +1,73 @@
+#include "time.h"
+#include "stdio.h"
+#include "stdlib.h"
 #include <iostream>
+using namespace std;
+int a[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//农历每月初一对应的2020年的第x天
+int b[] = {-5, 25, 54, 84, 114, 144, 173, 203, 232, 261, 291, 320, 350};
+
 class DateTime {
-private:
-  int seconds;     //从1970年1月1日0时开始计算的秒数 
-public:
-  DateTime();
-  DateTime(int y, int m, int d, int hour, int minute, int second); 
-  DateTime(const DateTime &dt);
-  ~DateTime();
-  void showTime();
-  void showMoon();  //作业：将当前公历转换为农历显示出来 
+	private:
+		int year, month, day;
+		int month_1, day_1;
+  	int hour, minute, second;
+  	int n_day;
+  public:
+  	DateTime();
+  	~DateTime();
+  	void showTime();
+  	void showMoon();
 };
 
-int main() {
-  DateTime dt, dt1(2020, 3, 27, 10, 40, 55);
-  DateTime dt2(dt), &dt3 = dt;
-  DateTime *dt4 = &dt;
-  dt.showTime();
-  dt1.showTime();
-  dt2.showTime();
-  dt3.showTime();
-  dt4->showTime();
-  return 0;
+int main()
+{
+	DateTime dt;
+	dt.showTime();
+	dt.showMoon();
+	return 0;
 }
+
 DateTime::DateTime()
 {
-}
-DateTime::DateTime(int y, int m, int d, int hour, int minute, int second)
-{
-}
-DateTime::DateTime(const DateTime &dt)
-{
+	cout << "仅支持到2020年末" << endl;
+	//得出系统当前时间 
+	time_t now;
+	struct tm *timenow;
+	time(&now);
+	timenow = localtime(&now);
+	year = timenow->tm_year+1900; month = timenow->tm_mon+1; day = timenow->tm_mday;
+	hour = timenow->tm_hour; minute = timenow->tm_min; second = timenow->tm_sec; 
 }
 DateTime::~DateTime() 
 {
-  std::cout << " Go die, Ha~Ha~" << std::endl;
+  cout << "Over" << endl;
 }
 void DateTime::showTime()
 {
-  printf("当前时间：%d/%d/%d %d:%d:%d\n", year, month, day, hour, minute, second);
+	printf("当前时间：%d/%d/%d %d:%d:%d\n",
+	year, month, day, hour, minute, second);
 }
- 
+void DateTime::showMoon()
+{
+	n_day = 0;
+	for (int i = 1; i < month; i++) {
+		n_day += a[i];
+	}
+	n_day += day;
+	for (month_1 = 0; b[month_1] <= n_day; month_1++) ;
+	month_1--;
+	day_1 = n_day - b[month_1] + 1;
+	if (month_1 == 0) {
+		printf("农历时间：2019/12/%d %d:%d:%d\n",
+		day_1, hour, minute, second);
+		return;
+	} else if (month_1 == 5) {
+		printf("农历时间：%d/闰四月/%d %d:%d:%d\n",
+		year, day_1, hour, minute, second);
+		return;
+	}
+	if (month_1 > 5) month_1--;
+	printf("农历时间：%d/%d/%d %d:%d:%d\n",
+	year, month_1, day_1, hour, minute, second);
+}
