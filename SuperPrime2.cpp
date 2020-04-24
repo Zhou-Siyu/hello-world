0f325a8d88dfc12a4457fdae8a7f6b5a59bedb99
Zhou-Siyu <1794278158@qq.com>
2020-04-24 22:14:22 +0800

    2020作业

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,5 +1,6 @@
 //作业：面向对象设计以下框架的代码细节，程序能编译运行得到正确结果 
 #include <iostream>
+//判断一个数是否为素数 
 class Prime {
   public:
   	Prime():number(0) {
@@ -9,67 +10,32 @@ class Prime {
 	~Prime() {
 	}
   	bool isPrime() { 
-  	  //2到number-1的因子 
-  	  return false;
+  	  for (int i = 2; i*i <= number; i++) {
+  	  	if (number%i == 0) {
+  	  		return false;
+				}
+			}
+			return true;
 	}
   private:
   	const int number;
 }; 
-class SuperPrime : public Prime {
-  public:
-  	SuperPrime():Prime(0), pset(3) {  //为什么必须有？ 
-  	}
-  	SuperPrime(int n):Prime(n), pset(3) {
-	  // number split into N
-	  int temp = n;
-	  while(temp > 0) {
-	  	int t = temp % 10;
-	  	temp /= 10;
-	  	pset.add(t);  //作业：单个数字为对象？还是和/积/平方和为对象？ 
-	  } 
-	}
-  	~SuperPrime() {
-	}
-  	bool isSuperPrime() {
-	  if (Prime::isPrime() && pset.isAllPrime())
-	    return true; 
-  	  return false;
-	}
-  private:
-  	PrimeSet pset;
-	int sum() {
-	  return 0;
-	}
-	int multi() {
-	  return 0;
-	}
-	int squareSum() {
-	  return 0;
-	}
-};
-class PrimeSet {
+//判断N中元素是否都为素数 
+class PrimeSet{
   public:
   	PrimeSet(int size) {
   	  //集合的构造什么？ 
   	  N = new Prime*[size];
-  	  this->size = size;
+//  	  this->size = size;
   	  index = 0;
 	}
 	~PrimeSet() {
-  	  for (int i = 0; i < index; ++i)  //销毁对象 
-		delete N[i]; 
+  	for (int i = 0; i < index; ++i)  //销毁对象 
+			delete N[i]; 
 	  delete[] N;
 	}
- 	int count() {
-  	  int count = 0;
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i]->isSuperPrime())
-  	      count += 1;
-	  return count; 
-	}
-
-	bool add(int n) {
-	  if(index == size)  return false;
+	bool add(int n) { 
+//	  if(index == size)  return false;
 	  Prime *p = new Prime(n);
 	  N[index] = p;
 	  index += 1;
@@ -82,39 +48,100 @@ class PrimeSet {
 	  return true;
 	} 
   private:
-  	Prime **set;
-	int size, index;
+  	Prime **N;
+		int size, index;
 };
-class SuperPrimeSet {
+//将一个数的各位数字按要求组合，并传入N，最后判断是否为超级素数 
+class SuperPrime : public Prime{
   public:
-  	SuperPrimeSet(int from, int to) {
-  	  size = to - from;
-  	  for (int i = from; i < to; i++)
-  	    set[i-from] = new SuperPrime(i);
-	}
-  	~SuperPrimeSet() {
-  	  for(int i = 0; i < size; i++)
-  	    delete set[i];
-	}
-  	int count() {
-  	  int count = 0;
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i]->isSuperPrime())
-  	      count += 1;
-	  return count; 
-	}
-  	int sum() {
-  	  int sum = 0;
-  	  /*
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i].isSuperPrime())
-  	      sum += set[i];
-  	      */ 
-	  return sum; 
-	}
+  	SuperPrime():Prime(0), pset(4) {  //为什么必须有？ 
+  	}
+  	SuperPrime(int n):Prime(n), pset(4) {
+			// number split into A
+			int i = 0;
+			int temp = n;
+		 	while(temp > 0) {
+	  		int t = temp % 10;
+			  temp /= 10;
+		  	A[i++] = t;  //作业：单个数字为对象？还是和/积/平方和为对象？
+			}
+			A[i] = -1;
+			sum();
+			multi();
+			squareSum();
+			pset.add(n);
+		}
+  	~SuperPrime() {
+		}
+  	bool isSuperPrime() {
+  	  //怎么使用pset？ 
+  	  /*Prime p(number);
+	  	if (p.isPrime())
+		    return true; 
+	  	return false;*/
+	  	if (pset.isAllPrime())
+	  		return true;
+	  	return false;
+		}
   private:
-  	SuperPrime **set;
-  	int size, index;
+  	int A[100];
+  	PrimeSet pset;
+		int sum() {
+			int n = 0, i = 0;
+			while(A[i] >= 0) {
+				n += A[i++];
+			}
+			pset.add(n);
+		  return 0;
+		}
+		int multi() {
+			int n = 1, i = 0;
+			while(A[i] >= 0) {
+				n *= A[i++];
+			}
+			pset.add(n);
+		  return 0;
+		}
+		int squareSum() {
+			int n = 0, i = 0;
+			while(A[i] >= 0) {
+				n += A[i]*A[i];
+				i++;
+			}
+			pset.add(n);
+		  return 0;
+		}
+};
+//求范围内超级素数的个数及它们的和 
+class SuperPrimeSet {
+	public:
+		SuperPrimeSet(int from, int to) {
+		  size = to - from + 1;
+		  this->from = from;
+		  for (int i = from; i <= to; i++)
+		    set[i - from] = new SuperPrime(i);
+		}
+		~SuperPrimeSet() {
+		  for(int i = 0; i < size; i++)
+		    delete set[i];
+		}
+		int count() {
+		  int count = 0;
+		  for (int i = 0; i < size; i++)
+		    if(set[i]->isSuperPrime())
+		      count += 1;
+	  	return count; 
+		}
+		int sum() {
+		  int sum = 0;
+		  for (int i = 0; i < size; i++)
+		    if(set[i]->isSuperPrime())
+		      sum += i+from;
+		  return sum; 
+		}
+	private:
+		SuperPrime *set[9999];
+		int size, from;
 };
 int main() {
   SuperPrime sp(113);
@@ -122,5 +149,9 @@ int main() {
     std::cout << "113 is SuperPrime" << std::endl;
   else
     std::cout << "113 is NOT SuperPrime" << std::endl;
+    
+  SuperPrimeSet ss(100, 999);
+  std::cout << "count:" << ss.count() << std::endl;
+  std::cout << "sum:" << ss.sum() <<  std::endl;
   return 0;
 }
